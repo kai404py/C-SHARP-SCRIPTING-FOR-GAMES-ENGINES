b09b112136dd2ae4ecb054efb9c1197e7598137a
Kai <Kaiboucher2007@gmail.com>
2026-01-20 13:14:56 +0000

    Update MovingObject movement logic and scene setup
    Refactored MovingObject.cs to move between start and end waypoints using Vector2.MoveTowards, and adjusted speed default to 1. Updated DevelopmentScene.unity to assign waypoints, reposition objects, and scale elements for improved scene layout and object movement demonstration.

--- CSharpForGamesFramework/Assets/Scripts/MovingObject.cs
+++ CSharpForGamesFramework/Assets/Scripts/MovingObject.cs
@@ -3,18 +3,39 @@ public class MovingObject : MonoBehaviour
 {
     [SerializeField] private Transform m_startWaypoint;
     [SerializeField] private Transform m_endWaypoint;
-    [SerializeField] private float m_speed = 5;
+    [SerializeField] private float m_speed = 1;
     
     private Transform m_target;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        m_target = m_startWaypoint;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 targetDestination = m_startWaypoint.position - transform.position;
+        bool goToEnd = true;
         
+        if (transform.position == m_startWaypoint.position)
+        {
+            goToEnd = false;
+        }
+        else
+        {
+            goToEnd = true;
+        }
+
+        if (goToEnd)
+        {
+            targetDestination = m_endWaypoint.position - transform.position;
+        }
+        else
+        {
+            targetDestination = m_startWaypoint.position - transform.position;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetDestination, m_speed * Time.deltaTime);
     }
 }
