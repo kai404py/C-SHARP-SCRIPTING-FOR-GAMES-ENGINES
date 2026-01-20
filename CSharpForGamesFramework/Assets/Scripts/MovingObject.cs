8876d30aeff90f0b7c90e57ce0b51274e570474e
Kai <Kaiboucher2007@gmail.com>
2026-01-20 13:57:56 +0000

    Refactor MovingObject to use trigger-based waypoint switching
    Updated MovingObject to switch between start and end waypoints using OnTriggerEnter2D instead of position checks in Update. Also refactored target management for clarity and adjusted DevelopmentScene to reposition a GameObject.

--- CSharpForGamesFramework/Assets/Scripts/MovingObject.cs
+++ CSharpForGamesFramework/Assets/Scripts/MovingObject.cs
@@ -4,38 +4,38 @@ public class MovingObject : MonoBehaviour
     [SerializeField] private Transform m_startWaypoint;
     [SerializeField] private Transform m_endWaypoint;
     [SerializeField] private float m_speed = 1;
-    
-    private Transform m_target;
+    bool goToEnd = true;
+
+    private Vector2 targetDestination;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        m_target = m_startWaypoint;
+        targetDestination = m_startWaypoint.position;
+    }
+
+    void ChangeTarget()
+    {
+        if (goToEnd == false)
+        {
+            goToEnd = true;
+            targetDestination = m_startWaypoint.position;
+        }
+        else
+        {
+            goToEnd = false;
+            targetDestination = m_endWaypoint.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ChangeTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetDestination = m_startWaypoint.position - transform.position;
-        bool goToEnd = true;
         
-        if (transform.position == m_startWaypoint.position)
-        {
-            goToEnd = false;
-        }
-        else
-        {
-            goToEnd = true;
-        }
-
-        if (goToEnd)
-        {
-            targetDestination = m_endWaypoint.position - transform.position;
-        }
-        else
-        {
-            targetDestination = m_startWaypoint.position - transform.position;
-        }
-
         transform.position = Vector2.MoveTowards(transform.position, targetDestination, m_speed * Time.deltaTime);
     }
 }
